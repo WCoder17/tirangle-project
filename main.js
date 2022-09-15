360db4e302d15a10cda066b900b69ee9d98a0fc3
airsCode <sirmryeti@gmail.com>
2022-09-15 09:57:56 -0600

    Initial Commit

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 // Triangle Perimeter Assignment Start Code
+
+// Button
+document.getElementById("calculateABC").addEventListener("click", btnClic);
+
+function btnClic() {
+  // Get Inputs for Triangle
+  let vertXA = +document.getElementById("vertxA").value;
+  let vertXB = +document.getElementById("vertxB").value;
+  let vertXC = +document.getElementById("vertxC").value;
+  let vertYA = +document.getElementById("vertyA").value;
+  let vertYB = +document.getElementById("vertyB").value;
+  let vertYC = +document.getElementById("vertyC").value;
+
+  // Calculate the triangle
+  document.getElementById("opAB").innerHTML = btnClic(vertXA, vertYA);
+  document.getElementById("opAB").innerHTML = btnClic(vertXB, vertYB);
+  document.getElementById("opAB").innerHTML = btnClic(vertXC, vertYC);
+
+  function calcTri(verttX, verttY) {
+  }
+}
