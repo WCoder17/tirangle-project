6a7685892e03aa70bc18b46a8c66d929f9575066
airsCode <sirmryeti@gmail.com>
2022-09-16 09:59:15 -0600

    updated

--- main.js
+++ main.js
@@ -5,18 +5,24 @@ document.getElementById("calculateABC").addEventListener("click", btnClic);
 
 function btnClic() {
   // Get Inputs for Triangle
-  let vertXA = +document.getElementById("vertxA").value;
-  let vertXB = +document.getElementById("vertxB").value;
-  let vertXC = +document.getElementById("vertxC").value;
-  let vertYA = +document.getElementById("vertyA").value;
-  let vertYB = +document.getElementById("vertyB").value;
-  let vertYC = +document.getElementById("vertyC").value;
+  let aX = +document.getElementById("vertxA").value;
+  let bX = +document.getElementById("vertxB").value;
+  let cX = +document.getElementById("vertxC").value;
+  let aY = +document.getElementById("vertyA").value;
+  let bY = +document.getElementById("vertyB").value;
+  let cY = +document.getElementById("vertyC").value;
 
   // Calculate the triangle
-  document.getElementById("opAB").innerHTML = btnClic(vertXA, vertYA);
-  document.getElementById("opAB").innerHTML = btnClic(vertXB, vertYB);
-  document.getElementById("opAB").innerHTML = btnClic(vertXC, vertYC);
+  document.getElementById("opAB").innerHTML = dist(aX, aY, bX, bY);
+  document.getElementById("opAC").innerHTML = dist(bX, bY, cX, cY);
+  document.getElementById("opBC").innerHTML = dist(cX, cY, aX, aY);
+  document.getElementById("opFin").innerHTML = dist(aB + aC + bC);
 
-  function calcTri(verttX, verttY) {
+  function dist(x1, y1, x2, y2) {
+    //aB = y2 - y1 / x2 - x1;
+    //aC = y2 - y1 / x2 - x1;
+    //bC = y2 - y1 / x2 - x1;
+    //periM = aB + aC + bC;
+    //return periM;
   }
 }
