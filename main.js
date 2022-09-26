222ae8b59c5cd41f9dc30d840cde12a7f5c315b2
airsCode <sirmryeti@gmail.com>
2022-09-26 10:00:23 -0600

    new commit

--- main.js
+++ main.js
@@ -11,12 +11,15 @@ function btnClic() {
   let aY = +document.getElementById("vertyA").value;
   let bY = +document.getElementById("vertyB").value;
   let cY = +document.getElementById("vertyC").value;
-  
 
   // Calculate the triangle
-  let aB = document.getElementById("opAB").innerHTML = dist(aX, aY, bX, bY);
-  let bC = document.getElementById("opBC").innerHTML = dist(bX, bY, cX, cY);
-  let aC = document.getElementById("opAC").innerHTML = dist(cX, cY, aX, aY);
+  //let aB = document.getElementById("opAB").innerHTML = dist(aX, aY, bX, bY);
+  //let bC = document.getElementById("opBC").innerHTML = dist(bX, bY, cX, cY);
+  //let aC = document.getElementById("opAC").innerHTML = dist(cX, cY, aX, aY);
+  let AB = dist(aX, aY, bX, bY);
+  let AC = dist(aX, aY, cX, cY);
+  let BC = dist(bX, bY, cX, cY);
+  let perimeter = AB + AC + BC;
 
   function dist(x1, y1, x2, y2) {
     return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
