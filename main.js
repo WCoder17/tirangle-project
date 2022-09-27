4947b3cb3e51d61fc9cb68e4012932328c1b5551
airsCode <sirmryeti@gmail.com>
2022-09-27 09:55:20 -0600

    Final Commit (I hope)
    Everything Works, not sure if its how its supposed to be coded but everything works.

--- main.js
+++ main.js
@@ -13,15 +13,18 @@ function btnClic() {
   let cY = +document.getElementById("vertyC").value;
 
   // Calculate the triangle
-  //let aB = document.getElementById("opAB").innerHTML = dist(aX, aY, bX, bY);
-  //let bC = document.getElementById("opBC").innerHTML = dist(bX, bY, cX, cY);
-  //let aC = document.getElementById("opAC").innerHTML = dist(cX, cY, aX, aY);
-  let AB = dist(aX, aY, bX, bY);
-  let AC = dist(aX, aY, cX, cY);
-  let BC = dist(bX, bY, cX, cY);
-  let perimeter = AB + AC + BC;
+  aB = document.getElementById("opAB").innerHTML = dist(aX, aY, bX, bY);
+  aC = document.getElementById("opAC").innerHTML = dist(aX, aY, cX, cY);
+  bC = document.getElementById("opBC").innerHTML = dist(bX, bY, cX, cY);
+  perimeter = aB + aC + bC;
+  opFin.innerHTML = perimeter;
+
+  //let aB = dist(aX, aY, bX, bY);
+  //let aC = dist(aX, aY, cX, cY);
+  //let bC = dist(bX, bY, cX, cY);
 
   function dist(x1, y1, x2, y2) {
     return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
   }
+  console.log(perimeter);
 }
